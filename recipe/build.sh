d0a1528b1c1886e439cfbfc85ba1503ac5ddc2ac
Silvio Traversaro <silvio@traversaro.it>
2023-02-20 23:37:20 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,16 @@
 mkdir build
 cd build
 
+# Workaround for https://github.com/conda-forge/tl-optional-feedstock/pull/3#issuecomment-1437631307
+if [[ "$(uname)" == "Darwin" && "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+    export TL_OPTIONAL_BUILD_TESTING=OFF
+else
+    export TL_OPTIONAL_BUILD_TESTING=ON
+fi
+
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_TESTING=ON
+      -DBUILD_TESTING=${TL_OPTIONAL_BUILD_TESTING}
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
